c17effbb769fc3425baa9dcc34a04170db159479
wen.you <wen.you@nextfortune.tw>
2021-11-13 23:37:28 +0800

    viectitle add icon

--- src/component/ViceTitle.vue
+++ src/component/ViceTitle.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="vice-title">
-    <div class="triangle"></div>
+    <div v-if="type===1" class="icon triangle"></div>
+    <div v-if="type===2" class="icon square"></div>
     <p>{{ title }}</p>
   </div>
 </template>
 
 <script>
+
 export default {
   props: {
     title: {
       type: String,
       default: "副標題",
     },
+    type:{
+      type: Number,
+      default: 1
+    }
   },
 };
 </script>
@@ -25,13 +31,22 @@ export default {
     margin-bottom: 20px;
 }
 
+.icon{
+  margin-right: 14px;
+}
+
+.square{
+  width: 20px;
+  height: 20px;
+  background-color: $primary-color-second;
+}
+
 .triangle {
   border-color: transparent transparent $primary-color-master transparent;
   border-style: solid solid solid solid;
   border-width: 0 14px 20px;
   height: 0px;
   width: 0px;
-  margin-right: 14px;
 }
 
 </style>
