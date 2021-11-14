3c34e6092b18e093fecafb27ee4839e128f2a5bd
wen.you <wen.you@nextfortune.tw>
2021-11-14 22:55:33 +0800

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/taiwan-travel/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/taiwan_travel/" : "/",
   css: {
     loaderOptions: {
       scss: {
